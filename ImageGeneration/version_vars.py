bd2c1f68434fb44b6969bf0adc414484778f1731
Tali Perry <tali.perry1@gmail.com>
2021-04-21 09:35:23 +0300

    npcm7xx: igps: version 2.1.17

--- ImageGeneration/version_vars.py
+++ ImageGeneration/version_vars.py
@@ -1,5 +1,5 @@
-bootblock_poleg_basic  = "Poleg_bootblock_basic.10.10.16.bin"
-bootblock_poleg_secure = "Poleg_bootblock_secure.10.10.16.bin"
+bootblock_poleg_basic  = "Poleg_bootblock_basic.10.10.17.bin"
+bootblock_poleg_secure = "Poleg_bootblock_secure.10.10.17.bin"
 uboot_poleg            = "u-boot_2019.01.7.5.bin"
 linux_uimage           = "uimage_4.17.4.01.03"
 linux_uRamdisk         = "uRamdisk_4.17.4.01.03"
